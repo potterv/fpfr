9f1d6af72865e706fdd4b99462265d008749d49e
Владимир Гончар <gonchar@092.pfr.ru>
2021-03-23 17:15:42 +0300

    test1

---
+++ src/main/java/ru/pfr/sev/cvp/rest/fpfr/FpfrApplication.java
@@ -0,0 +1,13 @@
+package ru.pfr.sev.cvp.rest.fpfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpfrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpfrApplication.class, args);
+    }
+
+}
